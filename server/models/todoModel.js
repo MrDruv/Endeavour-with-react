dd267cb3b885f9ebefb42cf285a72d8d4c10853c
MrDruv <druv0806@gmail.com>
2025-06-13 14:10:07 +0530

    postgres

--- server/models/todoModel.js
+++ server/models/todoModel.js
@@ -1,37 +1,53 @@
-const pool = require("../db");
+const { Pool } = require("pg");
 
-// Get all todos
+const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
+});
+
+module.exports = pool;
+
 const getAllTodos = async () => {
-  const result = await pool.query("SELECT * FROM todo ORDER BY id");
-  return result.rows;
+  const res = await pool.query("SELECT * FROM todo ORDER BY id");
+  return res.rows;
 };
 
-// Create a new todo
-const createTodo = async (text, completed, due_date, notes) => {
-  const result = await pool.query(
-    "INSERT INTO todo (text, completed, due_date, notes) VALUES ($1, $2, $3, $4) RETURNING *",
-    [text, completed, due_date, notes]
+const getTodoById = async (id) => {
+  const res = await pool.query("SELECT * FROM todo WHERE id = $1", [id]);
+  return res.rows[0];
+};
+
+const createTodo = async ({ text, completed, due_date, notes }) => {
+  const formattedDueDate = due_date === "" ? null : due_date;
+  const res = await pool.query(
+    `INSERT INTO todo (text, completed, due_date, notes) 
+     VALUES ($1, $2, $3, $4) RETURNING *`,
+    [text, completed, formattedDueDate, notes]
   );
-  return result.rows[0];
+  return res.rows[0];
 };
 
-// Update todo by ID
-const updateTodoById = async (id, text, completed, due_date, notes) => {
-  const result = await pool.query(
-    "UPDATE todo SET text = $1, completed = $2, due_date = $3, notes = $4 WHERE id = $5 RETURNING *",
-    [text, completed, due_date, notes, id]
+const updateTodo = async (id, { text, completed, due_date, notes }) => {
+  const formattedDueDate = due_date === "" ? null : due_date;
+  const res = await pool.query(
+    `UPDATE todo SET text = $1, completed = $2, due_date = $3, notes = $4 
+     WHERE id = $5 RETURNING *`,
+    [text, completed, formattedDueDate, notes, id]
   );
-  return result.rows;
+  return res.rows[0];
 };
 
-// Delete todo by ID
-const deleteTodoById = async (id) => {
-  return await pool.query("DELETE FROM todo WHERE id = $1", [id]);
+const deleteTodo = async (id) => {
+  await pool.query("DELETE FROM todo WHERE id = $1", [id]);
 };
 
 module.exports = {
   getAllTodos,
+  getTodoById,
   createTodo,
-  updateTodoById,
-  deleteTodoById,
+  updateTodo,
+  deleteTodo,
 };
