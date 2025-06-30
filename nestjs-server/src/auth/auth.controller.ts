47c5da7bf4fc2f9bcee44b06d33e9615195dc845
infranest7 <infranest7@gmail.com>
2025-06-30 18:59:27 +0530

    changes

--- nestjs-server/src/auth/auth.controller.ts
+++ nestjs-server/src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+// src/auth/auth.controller.ts
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login')
+  login(@Body() body: { email: string; password: string }) {
+    return this.authService.login(body.email, body.password);
+  }
+}
