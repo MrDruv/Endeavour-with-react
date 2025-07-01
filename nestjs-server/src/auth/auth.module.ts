38509c7a381bc36be77064ce914cc0331065843f
MrDruv <druv0806@gmail.com>
2025-07-01 14:15:42 +0530

    Update auth.module.ts

--- nestjs-server/src/auth/auth.module.ts
+++ nestjs-server/src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-// jwt
+
 @Module({
   imports: [
     JwtModule.register({
